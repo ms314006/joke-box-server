2e70d27c62ac474cada6e8e254edf6bdcea38f16
GQSM <ms314006@hotmail.com>
2020-06-10 15:33:50 +0800

    [ADD] Finished reading JSON file

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: deno run --allow-net=:${PORT} --allow-read --unstable index.ts --port=${PORT}
---
+++ index.ts
@@ -0,0 +1,21 @@
+import { serve } from 'https://deno.land/std@0.55.0/http/server.ts';
+import { readJsonSync } from 'https://deno.land/std/fs/mod.ts';
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+
+const { args } = Deno;
+const DEFAULT_PORT = 8000;
+const argPort = parse(args).port;
+
+const s = serve({ port: argPort ? Number(argPort) : DEFAULT_PORT });
+
+for await (const req of s) {
+  if (req.url === '/jokes') {
+    const jokes = readJsonSync('./jokes.json');
+    req.respond({
+      headers: new Headers({
+        'content-type': 'text/html;charset=UTF-8',
+      }),
+      body: JSON.stringify(jokes),
+    });
+  }
+}
